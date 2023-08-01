56ef99a4855b8560860d17b1657607c72afea9b5
NRH-AA <nheinz1995@live.com>
2023-08-01 06:41:52 -0700

    Create player depot items model

---
+++ app/models/player_depotitem.py
@@ -0,0 +1,26 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from flask_login import UserMixin
+from datetime import datetime
+
+class Player_Depot_Item(db.Model, UserMixin):
+    __tablename__ = 'player_depotitems'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+        
+    player_id = db.Column(db.Integer, nullable=False)
+    sid = db.Column(db.Integer, nullable=False)
+    pid = db.Column(db.Integer, nullable=False, default=0)
+    itemtype = db.Column(db.SmallInteger, nullable=False)
+    count = db.Column(db.SmallInteger, nullable=False, default=0)
+    attributes = db.Column(db.LargeBinary, nullable=False)
+    
+    def to_dict(self):
+        return {
+            'player_id': self.player_id,
+            'sid': self.sid,
+            'pid': self.pid,
+            'itemtype': self.itemtype,
+            'count': self.count,
+            'attributes': self.attributes
+        }
