e5ef0785f200bb83251e40334c145141c2390419
NRH-AA <nheinz1995@live.com>
2023-08-01 16:56:29 -0700

    Add flask_cors to enironment / remove uneeded code

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .models import db, User
 
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.player_routes import player_routes
 
 from .seeds import seed_commands
 
@@ -44,6 +45,7 @@ app.config.update(
 
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(player_routes, url_prefix='/api/player')
 
 # Connect db to app
 db.init_app(app)
