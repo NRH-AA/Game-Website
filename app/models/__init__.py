05bd6fd6246a330c341f2f5522f9893a148728fd
NRH-AA <nheinz1995@live.com>
2023-08-01 16:47:43 -0700

    Import models to __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,48 @@
-from .db import db
+from .db import db, environment, SCHEMA
 from .user import User
-from .db import environment, SCHEMA
+
+# Account
+from .account_ban_history import Account_Ban_History
+from .account_ban import Account_Ban
+from .account_storage import Account_Storage
+from .account_viplist import Account_Viplist
+from .account import Account
+
+# Guild
+from .guild_invite import Guild_Invite
+from .guild_membership import Guild_Membership
+from .guild_rank import Guild_Rank
+from .guild_war import Guild_War
+from .guild import Guild
+
+# House
+from .house_list import House_List
+from .house import House
+
+# Market
+from .market_history import Market_History
+from .market_offer import Market_Offer
+
+# Player
+from .player_death import Player_Death
+from .player_depotitem import Player_Depot_Item
+from .player_inboxitem import Player_Inbox_Item
+from .player_item import Player_Item
+from .player_namelock import Player_Namelock
+from .player_online import Players_Online
+from .player_spell import Player_Spell
+from .player_storage import Player_Storage
+from .player_storeinboxitems import Player_Store_Inbox_Item
+from .player import Player
+
+# IP
+from .ip_bans import Ip_Bans
+
+# Server
+from .server_config import Server_Config
+
+# Tile
+from .tile_store import Tile_Store
+
+# Town
+from .towns import Town
